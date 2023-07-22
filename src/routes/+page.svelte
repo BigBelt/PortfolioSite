791ab8800f01bdf28e5c2ec449e61228d35085e0
Jonathen Colon <made10223@gmail.com>
2023-07-21 20:28:30 -0400

    Changed from react to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<main class="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+    <div class="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <h1 class="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <span class="text-[hsl(280,100%,70%)]">Naberus Games</span>
+      </h1>
+      <h1 class="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <span class="text-[hsl(300,100%,70%)]">Coming Soon</span>
+      </h1>
+      <p class="text-2xl text-white">
+        Being made with ♥️ from The Naberus Games Team
+      </p>
+    </div>
+  </main>
